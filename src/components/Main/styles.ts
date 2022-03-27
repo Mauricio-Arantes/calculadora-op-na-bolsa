262127a0190502f5a363646c02923e590a04c3c8
Mauricio Arantes <noob-crispim@hotmail.com>
2022-03-27 20:53:21 -0300

    chore: layout background

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -2,9 +2,8 @@ import styled, { css } from 'styled-components';
 
 export const Container = styled.main`
   ${({ theme }) => css`
-    background-color: ${theme.colors.mainBg};
-    color: ${theme.colors.white};
-    width: 100%;
+    background-color: ${theme.colors.gray};
+    margin: 0 10% 0;
     height: 100vh;
     padding: ${theme.spacings.medium};
     text-align: center;
