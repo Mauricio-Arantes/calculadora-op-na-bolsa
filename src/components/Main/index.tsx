b6a4796dcac3bc636c831ee1f9737fb29e8b6a8a
Mauricio Arantes <noob-crispim@hotmail.com>
2022-03-27 21:19:08 -0300

    chore: modify main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,8 @@
 import * as S from './styles';
+import React from 'react';
 
-const Main = ({ title = 'Next boilerplate' }) => (
-  <S.Container>
-    <S.Title>{title}</S.Title>
-  </S.Container>
+const Main: React.FC = ({ children }) => (
+  <S.Container>{children || 'Storybook'}</S.Container>
 );
 
 export default Main;
