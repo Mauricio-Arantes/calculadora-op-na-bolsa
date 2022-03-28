496ca1866f4f05d9e9d8f91c87df38ac6af94be9
Mauricio Arantes <noob-crispim@hotmail.com>
2022-03-27 21:20:02 -0300

    chore: add font roboto

--- src/components/VerticalStepper/styles.ts
+++ src/components/VerticalStepper/styles.ts
@@ -1,17 +1,7 @@
 import styled, { css } from 'styled-components';
 
 export const Container = styled.main`
-  ${({ theme }) => css`
-    background-color: ${theme.colors.mainBg};
-    color: ${theme.colors.white};
-    width: 100%;
-    height: 100vh;
-    padding: ${theme.spacings.medium};
-    text-align: center;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  `}
+  text-align: left;
 `;
 
 export const Title = styled.h1`
