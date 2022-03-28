2ba2f89bd8454cb350463f33502b36323dda4c32
Mauricio Arantes <noob-crispim@hotmail.com>
2022-03-27 23:33:10 -0300

    chore: config step option

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,32 @@
 import Main from 'components/Main';
 import VerticalLinearStepper from '../components/VerticalStepper';
+import { TextField } from '@mui/material';
+import OptionButton from '../components/OptionButton';
 
-const steps = [
-  {
-    label: 'Select campaign settings',
-    body: `For each ad campaign that you create, you can control how much
-              you're willing to spend on clicks and conversions, which networks
-              and geographical locations you want your ads to show on, and more.`,
-  },
-  {
-    label: 'Create an ad group',
-    body: 'An ad group contains one or more ads which target a shared set of keywords.',
-  },
-  {
-    label: 'Create an ad',
-    body: `Try out different ad text to see what brings in the most customers,
+const Home = () => {
+  const steps = [
+    {
+      label: 'Nome da Ação',
+      body: <TextField id="stock-input-name" label="Ex: PETR4" />,
+    },
+    {
+      label: 'Tipo de operação',
+      body: <OptionButton />,
+    },
+    {
+      label: 'Mês de referência',
+      body: `Try out different ad text to see what brings in the most customers,
               and learn how to enhance your ads using features like ad extensions.
               If you run into any problems with your ads, find out how to tell if
               they're running and how to resolve approval issues.`,
-  },
-];
+    },
+  ];
 
-const Home = () => (
-  <Main>
-    <VerticalLinearStepper steps={steps} />
-  </Main>
-);
+  return (
+    <Main>
+      <VerticalLinearStepper steps={steps} />
+    </Main>
+  );
+};
 
 export default Home;
