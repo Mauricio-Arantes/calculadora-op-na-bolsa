e238ec8b68775ae53dc697d69bafedc65d88c4ff
Mauricio Arantes <noob-crispim@hotmail.com>
2022-04-01 00:27:09 -0300

    chore: create main component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,49 @@
 import Main from 'components/Main';
 import VerticalLinearStepper from '../components/VerticalStepper';
 import { TextField } from '@mui/material';
-import OptionButton from '../components/OptionButton';
 import MaterialUIPickers from '../components/DatePicker';
+import FormInputData from '../components/FormInputData';
+import { useState } from 'react';
+import { OperationType } from '../utils/operation/operation';
 
 const Home = () => {
+  const [mediumPrice, setMediumPrice] = useState(0);
+  const [mediumQuantity, setMediumQuantity] = useState(0);
+  const [accumulatedLoss, setAccumulatedLoss] = useState(0);
+  const [operation, setOperation] = useState([]);
+  const [referenceMonth, setReferenceMonth] = useState('');
+  const [formState, setFormState] = useState({
+    price: '',
+    quantity: '',
+    tax: '',
+    type: OperationType.BUY,
+  });
+  const [actionName, setActionName] = useState('');
+
   const steps = [
     {
       label: 'Nome da Ação',
-      body: <TextField id="stock-input-name" label="Ex: PETR4" />,
-    },
-    {
-      label: 'Tipo de operação',
-      body: <OptionButton />,
+      body: (
+        <TextField
+          value={actionName}
+          id="name"
+          label="Ex: PETR4"
+          onChange={e => setActionName(e.target.value)}
+        />
+      ),
     },
     {
       label: 'Mês de referência',
-      body: <MaterialUIPickers />,
+      body: (
+        <MaterialUIPickers
+          onChange={setReferenceMonth}
+          label={'Selecione o mês de referência'}
+        />
+      ),
     },
     {
       label: 'Cadastro dos dados',
-      body: <MaterialUIPickers />,
+      body: <FormInputData formState={formState} setFormState={setFormState} />,
     },
   ];
 
